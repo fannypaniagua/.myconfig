86785e3fa2dfcf64e2276930ccdd55cbc2485d71
Rafael Vega <email.rafa@gmail.com>
2012-12-11 10:28:39 -0500

    installing more dependencies automaticaaly for vim in linux

--- vim/open-with-vim.sh
+++ vim/open-with-vim.sh
@@ -2,18 +2,18 @@
 
 unamestr=`uname`
 # Mac OS
-# if [[ "$unamestr" == 'Darwin' ]]; then
-#    if [ $# != 0 ]; then
-#       exec mvim -v --servername VIM_RVG --remote-silent "$@"
-#    else 
-#       exec mvim -v --servername VIM_RVG
-#    fi
+if [[ "$unamestr" == 'Darwin' ]]; then
+   if [ $# != 0 ]; then
+      exec /usr/local/bin/vim --servername VIM_RVG --remote-silent "$@"
+   else 
+      exec /usr/local/bin/vim --servername VIM_RVG
+   fi
 # Linux
-# else
-if [ $# != 0 ]; then
-   exec /usr/local/bin/vim --servername VIM_RVG --remote-silent "$@"
-else 
-   exec /usr/local/bin/vim --servername VIM_RVG
+else
+   if [ $# != 0 ]; then
+      exec /usr/bin/vim --servername VIM_RVG --remote-silent "$@"
+   else 
+      exec /usr/bin/vim --servername VIM_RVG
+   fi
 fi
-# fi
 
