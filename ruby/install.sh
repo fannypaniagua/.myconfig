1114614774015898c15106bdf28d2ff589d1efe6
fannypaniagua <estefania@larepublicaciudadana.co>
2013-03-04 19:07:20 -0500

    Update install.sh

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-#git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
 
 # For MacOS
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+# echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+# echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 
 # For Ubuntu 
-# echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile
-# echo 'eval "$(rbenv init -)"' >> ~/.profile
+sudo apt-get install zlib1g-dev openssl libopenssl-ruby1.9.1 libssl-dev libruby1.9.1 libreadline-dev
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile
+echo 'eval "$(rbenv init -)"' >> ~/.profile
 
-#exec $SHELL -l
+exec $SHELL -l
 
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 rbenv install 1.9.3-p194
